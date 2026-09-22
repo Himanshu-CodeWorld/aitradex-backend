e2feb7457a843e9a89894c51fdf1accfb45d646d
CodeWithHimz <codewithhimz@gmail.com>
2026-09-22 12:21:19 +0530

    Added Sucess

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,16 +1,16 @@
+// src/config/firebaseAdmin.js
+
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
-  const serviceAccount = {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
-      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-      : undefined,
-  };
-
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+        : undefined,
+    }),
   });
 }
 
